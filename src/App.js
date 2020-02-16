84487a277208f08e6e203d69d23587bf12944e8d
priscilla <gracelin2009@yahoo.com.tw>
2020-02-16 22:58:26 +0800

    先做了一個大概

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import './App.css';
 import Table from './Table/Table';
 import common from './common';
 
-const fetch = () => {
-
-}
-
 class App extends Component {
-  state = {
-    data: []
+  constructor() {
+    super();
+
+    this.state = {
+      data: []
+    }
+
+    this.fetch();
   }
 
   fetch() {
     const api = 'https://raw.githubusercontent.com/hexschool/w3hexschool-API/master/data.json';
     new common().fetchData(api)
-      .then((d) => {
+      .then((res) => {
         this.setState({
-          data: d
+          data: res
         });
-        console.log(d);
       })
       .catch((err) => {
         console.log(err);
--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,31 +1,55 @@
 import React from 'react';
+import { Collapse } from 'antd';
+import 'antd/dist/antd.css';
 import './Table.css';
+
+const { Panel } = Collapse;
+
+const filterBlog = (data, url) => {
+    const filtered = data.filter((item) => {
+        return item.blogUrl === url;
+    });
+
+    return filtered;
+}
 
 const Table = (props) => {
     console.log(props.data);
+    const url = {};
+    for (let i = 0; i < props.data.length; i++) {
+        const blogUrl = props.data[i].blogUrl;
+        const name = props.data[i].name;
+        const updateTime = props.data[i].updateTime;
+
+        if (!url[blogUrl]) {
+            url[blogUrl] = {
+                name: name,
+                updateTime: updateTime
+            };
+        }
+    }
+
+    for (let blogurl in url) {
+        const filtered = filterBlog(props.data, blogurl);
+        url[blogurl].posts = filtered;
+    }
+
+    const allBlog = Object.keys(url);
     return (
-        <table className='Table'>
-            <tr>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Age</th>
-            </tr>
-            <tr>
-                <td>Jill</td>
-                <td>Smith</td>
-                <td>50</td>
-            </tr>
-            <tr>
-                <td>Eve</td>
-                <td>Jackson</td>
-                <td>94</td>
-            </tr>
-            <tr>
-                <td>John</td>
-                <td>Doe</td>
-                <td>80</td>
-            </tr>
-        </table>
+        <Collapse defaultActiveKey={['1']}>
+            {allBlog.map((item, index) => {
+                const title = url[item].name + '｜' + item + '｜' + url[item].updateTime;
+                return (
+                    <Panel header={title} key={index}>
+                        { url[item].posts.map((post, i) => {
+                            const title = post.blogList[0].title;
+                            const url = post.blogList[0].url;
+                            return <p key={i}>{title}{url}</p>
+                        }) }
+                    </Panel>
+                );
+            })}
+        </Collapse>
     );
 }
 

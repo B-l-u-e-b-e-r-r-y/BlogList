567aa6ebfa1ab3d3e6e086d3eff374cc6c8f0100
Priscilla <wl00535428@gmail.com>
2020-02-19 23:19:36 +0800

    Updates

---
+++ precache-manifest.1aab6a9aff38d747b64da713455b2240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6726e426bb6f32bc5df9bffa54f7c82",
+    "url": "/B-l-u-e-b-e-r-r-y/BlogList/index.html"
+  },
+  {
+    "revision": "1d31804c4945d5d2c4f9",
+    "url": "/B-l-u-e-b-e-r-r-y/BlogList/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "4da3d0a61cefe007c252",
+    "url": "/B-l-u-e-b-e-r-r-y/BlogList/static/css/main.6216532c.chunk.css"
+  },
+  {
+    "revision": "1d31804c4945d5d2c4f9",
+    "url": "/B-l-u-e-b-e-r-r-y/BlogList/static/js/2.58298b55.chunk.js"
+  },
+  {
+    "revision": "4da3d0a61cefe007c252",
+    "url": "/B-l-u-e-b-e-r-r-y/BlogList/static/js/main.638329a2.chunk.js"
+  },
+  {
+    "revision": "020b71094a04ea169038",
+    "url": "/B-l-u-e-b-e-r-r-y/BlogList/static/js/runtime-main.2c23dce3.js"
+  }
+]);
